57d4903f183ae5c23389a9d221250164029d38de
ZC <zhoucan1999@outlook.com>
2020-02-16 20:38:55 +0000

    Finish JUnit test for all the sort, all tests are passed

--- src/SortComparisonTest.java
+++ src/SortComparisonTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,23 +29,142 @@ public class SortComparisonTest
      * Check that the methods work for empty arrays
      */
     @Test
-    public void testEmpty()
-    {
+    public void testEmpty() {
+    	double[] a = null;
+    	
+    	// Test insertionSort
+    	a = SortComparison.insertionSort(a);
+    	assertNull("Test empty array for insertSort", a);
+    	
+    	// Test selectionSort
+    	a = null;
+    	a = SortComparison.selectionSort(a);
+    	assertNull("Test empty array for selectionSort", a);
+    	
+    	// Test quickSort
+    	a = null;
+    	a = SortComparison.quickSort(a);
+    	assertNull("Test empty array for quickSort", a);
+    	
+    	// Test mergeSortIterative
+    	a = null;
+    	a = SortComparison.mergeSortIterative(a);
+    	assertNull("Test empty array for mergeSortIterative", a);
+    	
+    	// Test mergeSortRecursive
+    	a = null;
+    	a = SortComparison.mergeSortRecursive(a);
+    	assertNull("Test empty array for mergeSortRecursive", a);
     }
-
-
-    // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
-    // be executed at least once from at least one test.
-
-    // ----------------------------------------------------------
-    /**
-     *  Main Method.
-     *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
-     *
-     */
-    public static void main(String[] args)
-    {
-        //TODO: implement this method
+    
+    @Test
+    public void testInsertionSort() {
+    	
+    	double[] a = {5, 2, 1, 3, 4};
+    	double[] a_sorted = {1, 2, 3, 4, 5};
+    	
+    	double[] b = {5.1, 2.5, 1.0, 3.4, 4.7};
+    	double[] b_sorted = {1.0, 2.5, 3.4, 4.7, 5.1};
+    	
+    	double[] c = {1.0};
+    	
+    	a = SortComparison.insertionSort(a);
+    	assertEquals("Test input as an integer array", a, a_sorted);
+    	
+    	b = SortComparison.insertionSort(b);
+    	assertEquals("Test input as a decimal array", b, b_sorted);
+    	
+    	c = SortComparison.insertionSort(c);
+    	assertEquals("Test input array only has one element", c, c);
+    	
+    }
+    
+    @Test
+    public void testSelectionSort() {
+    	
+    	double[] a = {5, 2, 1, 3, 4};
+    	double[] a_sorted = {1, 2, 3, 4, 5};
+    	
+    	double[] b = {5.1, 2.5, 1.0, 3.4, 4.7};
+    	double[] b_sorted = {1.0, 2.5, 3.4, 4.7, 5.1};
+    	
+    	double[] c = {1.0};
+    	
+    	a = SortComparison.selectionSort(a);
+    	assertEquals("Test input as an integer array", a, a_sorted);
+    	
+    	b = SortComparison.selectionSort(b);
+    	assertEquals("Test input as a decimal array", b, b_sorted);
+    	
+    	c = SortComparison.selectionSort(c);
+    	assertEquals("Test input array only has one element", c, c);
+    	
+    }
+    
+    @Test
+    public void testQuickSort() {
+    	
+    	double[] a = {5, 2, 1, 3, 4};
+    	double[] a_sorted = {1, 2, 3, 4, 5};
+    	
+    	double[] b = {5.1, 2.5, 1.0, 3.4, 4.7};
+    	double[] b_sorted = {1.0, 2.5, 3.4, 4.7, 5.1};
+    	
+    	double[] c = {1.0};
+    	
+    	a = SortComparison.quickSort(a);
+    	assertEquals("Test input as an integer array", a, a_sorted);
+    	
+    	b = SortComparison.quickSort(b);
+    	assertEquals("Test input as a decimal array", b, b_sorted);
+    	
+    	c = SortComparison.quickSort(c);
+    	assertEquals("Test input array only has one element", c, c);
+    	
+    }
+    
+    @Test
+    public void testMergeSortIterative() {
+    	
+    	double[] a = {5, 2, 1, 3, 4};
+    	double[] a_sorted = {1, 2, 3, 4, 5};
+    	
+    	double[] b = {5.1, 2.5, 1.0, 3.4, 4.7};
+    	double[] b_sorted = {1.0, 2.5, 3.4, 4.7, 5.1};
+    	
+    	double[] c = {1.0};
+    	
+    	a = SortComparison.mergeSortIterative(a);
+    	assertEquals("Test input as an integer array", a, a_sorted);
+    	
+    	b = SortComparison.mergeSortIterative(b);
+    	assertEquals("Test input as a decimal array", b, b_sorted);
+    	
+    	c = SortComparison.mergeSortIterative(c);
+    	assertEquals("Test input array only has one element", c, c);
+    	
+    }
+    
+    @Test
+    public void testMergeSortRecursive() {
+    	
+    	double[] a = {5, 2, 1, 3, 4};
+    	double[] a_sorted = {1, 2, 3, 4, 5};
+    	
+    	double[] b = {5.1, 2.5, 1.0, 3.4, 4.7};
+    	double[] b_sorted = {1.0, 2.5, 3.4, 4.7, 5.1};
+    	
+    	double[] c = {1.0};
+    	
+    	a = SortComparison.mergeSortRecursive(a);
+    	assertEquals("Test input as an integer array", a, a_sorted);
+    	
+    	b = SortComparison.mergeSortRecursive(b);
+    	assertEquals("Test input as a decimal array", b, b_sorted);
+    	
+    	c = SortComparison.mergeSortRecursive(c);
+    	assertEquals("Test input array only has one element", c, c);
+    	
     }
 
 }
